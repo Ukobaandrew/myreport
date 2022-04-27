35f49d905b12143239363e4c302b5bd62c3ab15b
Ukobaandrew <101108667+Ukobaandrew@users.noreply.github.com>
2022-04-27 15:12:42 +0100

    upload files

---
+++ andy.js
@@ -0,0 +1,21 @@
+// let numbers = 0;
+// while (numbers <= 100) {
+//   if (numbers = numbers + 1) {
+//     console.log(numbers + "is odd");
+//   } else if ((numbers = numbers + 2)) {
+//     console.log(numbers + "is even");
+//   }
+// }
+
+// let num = 0;
+// let output = " is even";
+// let star = " is odd";
+// while (num <= 100) {
+//   if (num % 2 == 0) {
+//     console.log(num + output);
+//   } else {
+//     console.log(num + star);
+//   }
+//   num++;
+// }
+//
